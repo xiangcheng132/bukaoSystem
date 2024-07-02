757a9777666869b140aae84461feb7396800d000
zhenglou <userEmail>
2024-07-02 15:35:06 +0800

    ROUTER CHANGE

--- bukaovue/src/router/index.js
+++ bukaovue/src/router/index.js
@@ -1,19 +1,35 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import Login from "@/views/login.vue"
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    redirect:'/login',
+    name: 'index',
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import( '@/views/register.vue')
+  },
+  {
+    path: '/student',
+    name: 'student',
+    component:() => import( '@/views/student/index.vue')
+  },
+  {
+    path: '/teacher',
+    name: 'teacher',
+    component:() => import( '@/views/teacher/index.vue')
+  },
+  {
+    path: '/manager',
+    name: 'manager',
+    component:() => import( '@/views/manager/index.vue')
   }
 ]
 
