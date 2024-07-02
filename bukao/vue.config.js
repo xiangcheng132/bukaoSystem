bf5662c7fe39c16de025e231e4b5d84f74477fc5
xiangcheng132 <81361483+xiangcheng132@users.noreply.github.com>
2024-07-02 14:40:06 +0800

    Update vue.config.js

--- bukao/vue.config.js
+++ bukao/vue.config.js
@@ -1,4 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  devServer:{
+    proxy:{
+      //自定义请求的开头
+      "/api":{
+        //目标地址
+        target:"http://localhost:8080",
+        //因为/api不是请求的一部分，所以替换；
+        pathRewrite:{
+          "^/api":"",
+        }
+      }
+    }
+  }
+});
