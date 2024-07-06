c4764a418cc43292f66c0b749605548bfc4409c6
zhenglou <97831862+zhenglou@users.noreply.github.com>
2024-07-06 16:41:36 +0800

    vuex user模块userInfo自动刷新

---
+++ bukaovue/src/utils/userInfoRefrsh.js
@@ -0,0 +1,15 @@
+import { getStorage } from "./storage";
+import { getAllUserById } from "@/api/examUser";
+import store from "@/store"
+export function userInfoRefrsh(token) {
+  console.log(token);
+    getAllUserById(getStorage(token)).then(res=>{
+      // console.log(store);
+      store.commit("user/setUserInfo",res.data)
+      console.log("用户信息刷新成功");
+      return;
+    }).catch(err=>{
+      // console.log(err);
+      return "";
+    })
+}
