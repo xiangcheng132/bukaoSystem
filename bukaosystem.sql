c71ccae3bbb23cb16f89321cf6c7c84c9c7a1606
820574911@qq.com <820574911@qq.com>
2024-10-24 21:38:42 +0800

    2024/10/24数据库文件更新

--- bukaosystem.sql
+++ bukaosystem.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80023
  File Encoding         : 65001
 
- Date: 02/07/2024 14:30:49
+ Date: 24/10/2024 21:34:36
 */
 
 SET NAMES utf8mb4;
@@ -29,16 +29,21 @@ CREATE TABLE `exam_answer_sheet`  (
   `createTime` datetime NULL DEFAULT CURRENT_TIMESTAMP,
   `isGraded` int UNSIGNED NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `idx_exam_user`(`examId`, `userId`) USING BTREE,
   INDEX `as_userId`(`userId`) USING BTREE,
-  INDEX `examId`(`examId`) USING BTREE,
   CONSTRAINT `as_userId` FOREIGN KEY (`userId`) REFERENCES `exam_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `exam_answer_sheet_ibfk_1` FOREIGN KEY (`examId`) REFERENCES `exam_exam` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 23 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of exam_answer_sheet
 -- ----------------------------
-INSERT INTO `exam_answer_sheet` VALUES (1, 2, 3, 6, '2024-07-02 10:59:18', 1);
+INSERT INTO `exam_answer_sheet` VALUES (1, 2, 3, 80, '2024-09-04 00:55:51', 1);
+INSERT INTO `exam_answer_sheet` VALUES (5, 2, 4, 89, '2024-09-05 00:13:28', 0);
+INSERT INTO `exam_answer_sheet` VALUES (10, 2, 5, 91, '2024-09-05 00:24:04', 0);
+INSERT INTO `exam_answer_sheet` VALUES (18, 2, 6, 98, '2024-09-05 00:24:56', 0);
+INSERT INTO `exam_answer_sheet` VALUES (21, 2, 8, 99, '2024-09-05 00:25:57', 0);
+INSERT INTO `exam_answer_sheet` VALUES (23, 2, 9, 99, '2024-09-05 00:26:02', 0);
 
 -- ----------------------------
 -- Table structure for exam_answer_sheet_detail
@@ -52,19 +57,21 @@ CREATE TABLE `exam_answer_sheet_detail`  (
   `isTrue` enum('对','错','半对半错','未知') CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT '未知',
   `createTime` datetime NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `asd_answerId`(`answerId`) USING BTREE,
+  UNIQUE INDEX `idx_answerId_resourceId`(`answerId`, `resourceId`) USING BTREE,
   INDEX `asd_resourceId`(`resourceId`) USING BTREE,
   CONSTRAINT `asd_answerId` FOREIGN KEY (`answerId`) REFERENCES `exam_answer_sheet` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `asd_resourceId` FOREIGN KEY (`resourceId`) REFERENCES `exam_resources` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of exam_answer_sheet_detail
 -- ----------------------------
 INSERT INTO `exam_answer_sheet_detail` VALUES (1, 1, 1, 'ces', '对', '2024-07-02 10:59:34');
-INSERT INTO `exam_answer_sheet_detail` VALUES (2, 1, 2, '123', '对', '2024-07-02 11:00:57');
+INSERT INTO `exam_answer_sheet_detail` VALUES (2, 1, 2, '80', '对', '2024-09-04 01:08:00');
 INSERT INTO `exam_answer_sheet_detail` VALUES (3, 1, 3, 'ce', '对', '2024-07-02 11:01:08');
-INSERT INTO `exam_answer_sheet_detail` VALUES (4, 1, 4, 'ce', '错', '2024-07-02 11:01:14');
+INSERT INTO `exam_answer_sheet_detail` VALUES (4, 1, 4, '谢', '错', '2024-09-04 01:08:46');
+INSERT INTO `exam_answer_sheet_detail` VALUES (8, 1, 5, '谢', '未知', '2024-09-05 00:13:08');
+INSERT INTO `exam_answer_sheet_detail` VALUES (10, 1, 6, '5555', '未知', '2024-09-04 01:18:27');
 
 -- ----------------------------
 -- Table structure for exam_class
@@ -135,13 +142,16 @@ CREATE TABLE `exam_course`  (
   `comment` text CHARACTER SET utf8 COLLATE utf8_bin NULL,
   `createTime` datetime NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of exam_course
 -- ----------------------------
 INSERT INTO `exam_course` VALUES (1, 'course1', NULL, '2024-06-29 09:46:26');
 INSERT INTO `exam_course` VALUES (2, 'course2', NULL, '2024-06-29 09:46:39');
+INSERT INTO `exam_course` VALUES (4, '计算机导论', NULL, '2024-07-08 09:12:51');
+INSERT INTO `exam_course` VALUES (5, '计算机导论2', NULL, '2024-07-08 09:12:56');
+INSERT INTO `exam_course` VALUES (6, '计算机导论3', NULL, '2024-07-08 09:21:09');
 
 -- ----------------------------
 -- Table structure for exam_course_chapter
@@ -154,7 +164,7 @@ CREATE TABLE `exam_course_chapter`  (
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `cc_crouseId`(`courseId`) USING BTREE,
   CONSTRAINT `cc_crouseId` FOREIGN KEY (`courseId`) REFERENCES `exam_course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of exam_course_chapter
@@ -177,11 +187,14 @@ CREATE TABLE `exam_course_class`  (
   INDEX `cc_courseId`(`courseId`) USING BTREE,
   CONSTRAINT `cc_classId` FOREIGN KEY (`classId`) REFERENCES `exam_class` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `cc_courseId` FOREIGN KEY (`courseId`) REFERENCES `exam_course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of exam_course_class
 -- ----------------------------
+INSERT INTO `exam_course_class` VALUES (1, 1, 1, '2024-09-03 16:15:25');
+INSERT INTO `exam_course_class` VALUES (2, 1, 2, '2024-09-03 16:15:31');
+INSERT INTO `exam_course_class` VALUES (3, 2, 2, '2024-09-03 16:15:40');
 
 -- ----------------------------
 -- Table structure for exam_exam
@@ -200,12 +213,14 @@ CREATE TABLE `exam_exam`  (
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `exam_courseId`(`courseId`) USING BTREE,
   CONSTRAINT `exam_courseId` FOREIGN KEY (`courseId`) REFERENCES `exam_course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of exam_exam
 -- ----------------------------
-INSERT INTO `exam_exam` VALUES (2, 1, 'exam2', 'test2', NULL, 0, '2024-07-01 14:12:05', '2024-07-02 10:12:07', '2024-07-01 14:12:11');
+INSERT INTO `exam_exam` VALUES (2, 1, 'exam2', 'test2', '1', 0, '2024-07-01 14:12:05', '2024-07-02 10:12:07', '2024-07-01 14:12:11');
+INSERT INTO `exam_exam` VALUES (3, 2, 'exam3', 'exam3', NULL, 0, '2024-08-27 08:44:57', '2024-08-29 08:45:02', '2024-08-19 08:45:04');
+INSERT INTO `exam_exam` VALUES (4, 4, 'exam4', 'exam4', NULL, 0, '2024-08-26 08:45:17', '2024-08-23 08:45:20', '2024-08-19 08:45:22');
 
 -- ----------------------------
 -- Table structure for exam_exam_class
@@ -221,13 +236,15 @@ CREATE TABLE `exam_exam_class`  (
   INDEX `ecMid_examId`(`examId`) USING BTREE,
   CONSTRAINT `ecMid_classId` FOREIGN KEY (`classId`) REFERENCES `exam_class` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `ecMid_examId` FOREIGN KEY (`examId`) REFERENCES `exam_exam` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of exam_exam_class
 -- ----------------------------
 INSERT INTO `exam_exam_class` VALUES (2, 2, 1, '2024-07-01 14:13:15');
 INSERT INTO `exam_exam_class` VALUES (3, 2, 2, '2024-07-01 14:13:37');
+INSERT INTO `exam_exam_class` VALUES (4, 3, 1, '2024-08-19 08:45:37');
+INSERT INTO `exam_exam_class` VALUES (5, 4, 1, '2024-08-19 08:45:45');
 
 -- ----------------------------
 -- Table structure for exam_exam_resources
@@ -243,7 +260,7 @@ CREATE TABLE `exam_exam_resources`  (
   INDEX `erMid_resourceId`(`resourceId`) USING BTREE,
   CONSTRAINT `erMid_examId` FOREIGN KEY (`examId`) REFERENCES `exam_exam` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `erMid_resourceId` FOREIGN KEY (`resourceId`) REFERENCES `exam_resources` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of exam_exam_resources
@@ -266,14 +283,14 @@ CREATE TABLE `exam_resources`  (
   `options` json NULL,
   `key` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `analysis` text CHARACTER SET utf8 COLLATE utf8_bin NULL,
-  `score` int NULL DEFAULT 2,
+  `score` double NULL DEFAULT 2,
   `createTime` datetime NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `re_chapter`(`chapterId`) USING BTREE,
   INDEX `re_courseId`(`courseId`) USING BTREE,
   CONSTRAINT `re_chapter` FOREIGN KEY (`chapterId`) REFERENCES `exam_course_chapter` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `re_courseId` FOREIGN KEY (`courseId`) REFERENCES `exam_course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of exam_resources
@@ -281,7 +298,14 @@ CREATE TABLE `exam_resources`  (
 INSERT INTO `exam_resources` VALUES (1, 1, 1, 'test', 'bigquestion', NULL, 'ces', NULL, 2, '2024-07-02 10:57:29');
 INSERT INTO `exam_resources` VALUES (2, 1, 2, 'test2', 'bigquestion', NULL, '123', NULL, 2, '2024-07-02 10:57:41');
 INSERT INTO `exam_resources` VALUES (3, 1, 2, 'test3', 'bigquestion', NULL, 'ce', NULL, 2, '2024-07-02 10:57:54');
-INSERT INTO `exam_resources` VALUES (4, 1, 3, 'test4', 'bigquestion', NULL, '12', NULL, 3, '2024-07-02 10:58:10');
+INSERT INTO `exam_resources` VALUES (4, 1, 3, 'test4', 'bigquestion', '1', '12', NULL, 3, '2024-07-02 10:58:10');
+INSERT INTO `exam_resources` VALUES (5, 1, 2, 'What is the capital of France?', 'single_choice', '{\"A\": \"Berlin\", \"B\": \"Madrid\", \"C\": \"Paris\", \"D\": \"Rome\"}', 'C', 'The capital of France is Paris.', 5, NULL);
+INSERT INTO `exam_resources` VALUES (6, 1, 2, 'What is the capital of France?', 'single_choice', '{\"A\": \"Berlin\", \"B\": \"Madrid\", \"C\": \"Paris\", \"D\": \"Rome\"}', 'C', 'The capital of France is Paris.', 5, NULL);
+INSERT INTO `exam_resources` VALUES (7, 1, 2, 'What is the capital of France?', NULL, NULL, 'C', 'The capital of France is Paris.', 5, NULL);
+INSERT INTO `exam_resources` VALUES (8, 1, 2, 'This is a question', 'single_choice', '{\"A\": \"是一个好东西\", \"B\": \"是一个坏东西\", \"C\": \"是一个不好不坏东西\", \"D\": \"以上都不是\"}', 'A', 'This is an analysis', 5, '2023-07-03 00:00:00');
+INSERT INTO `exam_resources` VALUES (9, 1, 2, 'This is a question', 'single_choice', '{\"A\": \"是一个好东西\", \"B\": \"是一个坏东西\", \"C\": \"是一个不好不坏东西\", \"D\": \"以上都不是\"}', 'A', 'This is an analysis', 5, '2023-07-03 00:00:00');
+INSERT INTO `exam_resources` VALUES (10, 1, 2, 'This is a question', 'single_choice', '{\"A\": \"是一个好东西\", \"B\": \"是一个坏东西\", \"C\": \"是一个不好不坏东西\", \"D\": \"以上都不是\"}', 'A', 'This is an analysis', 5, '2023-07-03 00:00:00');
+INSERT INTO `exam_resources` VALUES (11, 1, 2, 'This is a question', 'single_choice', '{\"A\": \"是一个好东西\", \"B\": \"是一个坏东西\", \"C\": \"是一个不好不坏东西\", \"D\": \"以上都不是\"}', 'A', 'This is an analysis', 5, '2023-07-03 00:00:00');
 
 -- ----------------------------
 -- Table structure for exam_teacher_course
@@ -297,12 +321,15 @@ CREATE TABLE `exam_teacher_course`  (
   INDEX `tc_teacherId`(`teacherId`) USING BTREE,
   CONSTRAINT `tc_crouseId` FOREIGN KEY (`courseId`) REFERENCES `exam_course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `tc_teacherId` FOREIGN KEY (`teacherId`) REFERENCES `exam_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of exam_teacher_course
 -- ----------------------------
 INSERT INTO `exam_teacher_course` VALUES (3, 2, 1, '2024-07-01 15:33:27');
+INSERT INTO `exam_teacher_course` VALUES (4, 2, 2, '2024-08-19 08:43:58');
+INSERT INTO `exam_teacher_course` VALUES (5, 2, 4, '2024-08-19 08:44:03');
+INSERT INTO `exam_teacher_course` VALUES (6, 2, 5, '2024-08-19 08:44:08');
 
 -- ----------------------------
 -- Table structure for exam_user
@@ -319,16 +346,17 @@ CREATE TABLE `exam_user`  (
   `sex` enum('男','女','未知') CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT '未知',
   `createTime` datetime NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of exam_user
 -- ----------------------------
-INSERT INTO `exam_user` VALUES (2, 'test3', 'test3', '123456', 'teacher', '123456789@qq.com', '15088949856', '未知', '2024-06-28 16:54:43');
+INSERT INTO `exam_user` VALUES (2, 'test8', 'test8', '123456123', 'teacher', '123456789@qq.com', '15088949856', '女', '2024-06-28 16:54:43');
 INSERT INTO `exam_user` VALUES (3, 'test4', 'test4', '123456', 'student', '123456789@qq.com', '15088949856', '未知', '2024-06-28 16:54:46');
 INSERT INTO `exam_user` VALUES (4, 'test5', 'test5', '123456', 'student', '123456789@qq.com', '15088949856', '未知', '2024-06-28 16:58:56');
 INSERT INTO `exam_user` VALUES (5, 'test1', 'test1', '123456', 'student', '123456789@qq.com', '15088949856', '女', '2024-06-28 18:28:35');
 INSERT INTO `exam_user` VALUES (6, 'test2', 'test2', '123456', 'teacher', '123456789@qq.com', '15088949856', '未知', '2024-06-28 18:30:37');
 INSERT INTO `exam_user` VALUES (8, 'test4', 'test23', 'password456', 'teacher', 'jane.doe@example.com', '987-654-3210', '男', '2023-02-15 09:30:00');
+INSERT INTO `exam_user` VALUES (9, 'test8', 'test8', '123456', 'teacher', '123456789@qq.com', '15088949856', '女', '2024-06-28 16:54:43');
 
 SET FOREIGN_KEY_CHECKS = 1;
